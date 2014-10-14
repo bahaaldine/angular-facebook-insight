5993a1e3d5ed6e46a4df20fbf53d73cf7ba84578
Bahaaldine Azarmi <bahaaldine@gmail.com>
2014-10-14 11:34:50 +0200

    added missing dist

--- dist/angular-facebook-insight.js
+++ dist/angular-facebook-insight.js
@@ -1,4 +1,4 @@
-/*! angular-facebook-insight - v0.6.5 - 2014-10-14
+/*! angular-facebook-insight - v0.6.7 - 2014-10-14
 * Copyright (c) 2014 ; Licensed  */
   /*! angular-facebook-insight - v0.6.1 - 2014-07-13
 * Copyright (c) 2014 ; Licensed  */
@@ -31,7 +31,7 @@ angular.module("angular-facebook-insight-tpls",
 var page_id = 0;
 
 angular.module("angular-facebook-insight",
-  ["nvd3ChartDirectives", "googlechart", "r5.chart.directives"])
+  ["nvd3ChartDirectives", "googlechart"])
 
 .service('Parser', ['$filter', function($filter) {
   return {
