82209edecb7a297261c46435084ae661c65029e5
Bahaaldine Azarmi <bahaaldine@gmail.com>
2014-11-03 13:38:11 +0100

    dist

--- dist/angular-facebook-insight.js
+++ dist/angular-facebook-insight.js
@@ -1,4 +1,4 @@
-/*! angular-facebook-insight - v0.6.10 - 2014-11-03
+/*! angular-facebook-insight - v0.6.11 - 2014-11-03
 * Copyright (c) 2014 ; Licensed  */
   /*! angular-facebook-insight - v0.6.1 - 2014-07-13
 * Copyright (c) 2014 ; Licensed  */
@@ -17,15 +17,6 @@ angular.module("angular-facebook-insight-tpls",
 /*! angular-facebook-insight - v0.6.4 - 2014-08-07
 * Copyright (c) 2014 ; Licensed  */
 'use strict';
-
-angular.module("angular-facebook-insight-tpls", 
-  ["templates/fb-insight-dailyLine.html"
-  ,"templates/fb-insight-feedbackBreakdown.html"
-  ,"templates/fb-insight-genderBreakdown.html"
-  ,"templates/fb-insight-geomap.html"
-  ,"templates/fb-insight-hourlyLine"
-  ,"templates/fb-insight-map.html"
-  ,"templates/fb-insight-text.html"]);
 
 angular.module("angular-facebook-insight",
   ["nvd3ChartDirectives", "googlechart"])
