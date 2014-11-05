dd0a410b7391d4360bcb277e37fae4514feea003
Bahaaldine Azarmi <bahaaldine@gmail.com>
2014-11-05 23:30:27 +0100

    dist

--- dist/angular-facebook-insight.js
+++ dist/angular-facebook-insight.js
@@ -1,10 +1,10 @@
-/*! angular-facebook-insight - v0.6.22 - 2014-11-05
+/*! angular-facebook-insight - v0.6.23 - 2014-11-05
 * Copyright (c) 2014 ; Licensed  */
   /*! angular-facebook-insight - v0.6.1 - 2014-07-13
 * Copyright (c) 2014 ; Licensed  */
 'use strict';
 
-angular.module("angular-facebook-insight-tpls", 
+angular.module("angular-facebook-insight-tpls"
   ,"templates/fb-insight-geomap.html"
   ,"templates/fb-insight-map.html"
   ,"templates/fb-insight-pie.html"
