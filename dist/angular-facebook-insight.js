b2cd470afb873d418895c7631f60968e6d322723
Bahaaldine Azarmi <bahaaldine@gmail.com>
2014-11-07 21:16:48 +0100

    changed fixed image size to relative

--- dist/angular-facebook-insight.js
+++ dist/angular-facebook-insight.js
@@ -1,4 +1,4 @@
-/*! angular-facebook-insight - v0.6.43 - 2014-11-07
+/*! angular-facebook-insight - v0.6.44 - 2014-11-07
 * Copyright (c) 2014 ; Licensed  */
   /*! angular-facebook-insight - v0.6.1 - 2014-07-13
 * Copyright (c) 2014 ; Licensed  */
